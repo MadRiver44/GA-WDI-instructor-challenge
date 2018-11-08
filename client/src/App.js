0ce20c79b6a3599579149e2c3da42909ec0ff977
Kevin Turney <super996@optonline.net>
2018-11-08 14:36:35 -0500

    handling methods for fetching data, view res in console

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import './App.scss';
 import Search from './components/Search';
+import Movies from './components/Movies';
+//import axios from 'axios';
 
-const API = `http://www.omdbapi.com/?t=`;
-const API_KEY = `apikey=21d400e3`;
+// const API = `http://www.omdbapi.com/?apikey=`;
+// const API_KEY = `apikey=21d400e3`;
 class App extends Component {
   constructor(props) {
     super(props);
@@ -12,16 +14,42 @@ class App extends Component {
       data: [],
     };
 
-    /*this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleClick = this.handleClick.bind(this);*/
+    this.handleChange = this.handleChange.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  getMovies() {
+    fetch(`http://www.omdbapi.com/?s=${this.state.query}&apikey=21d400e3`)
+      .then(res => res.json())
+      .catch(error => {
+        console.log('Error fetching data', error);
+      })
+      .then(res => {
+        //console.log(res.Search);
+        this.setState({ data: res.Search });
+      });
+  }
+
+  handleChange(e) {
+    this.setState({ query: e.target.value });
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+    this.getMovies();
   }
 
   render() {
     return (
       <div className="App">
         <header className="title">Welcome to WDI Movie App</header>
-        <Search />
+        <Search
+          handleClick={this.handleClick}
+          handleChange={this.handleChange}
+          value={this.state.query}
+        />
+        <Movies items={this.state.data} />
       </div>
     );
   }
