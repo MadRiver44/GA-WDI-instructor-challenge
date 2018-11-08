7c0cb2df66609a126a96ba8f7d67eefc5b5457da
Kevin Turney <super996@optonline.net>
2018-11-08 13:42:16 -0500

    delete/ modify files for scss

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Search from './components/Search';
 
+const API = `http://www.omdbapi.com/?t=`;
+const API_KEY = `apikey=21d400e3`;
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: '',
+      data: [],
+    };
+
+    /*this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleClick = this.handleClick.bind(this);*/
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <header className="title">Welcome to WDI Movie App</header>
+        <Search />
       </div>
     );
   }
