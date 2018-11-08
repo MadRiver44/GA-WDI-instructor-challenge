a84a14a9a602d1c61a99f282f56a1eb931e39a4d
Kevin Turney <super996@optonline.net>
2018-11-08 16:30:53 -0500

    modify Movies.js for Movie.js

--- client/src/components/Movies.js
+++ client/src/components/Movies.js
@@ -1,15 +1,28 @@
 import React from 'react';
+import Movie from './Movie';
 
 const Movies = props => {
-  const { items } = props;
+  console.log(props);
+  const { items, getMovieInfo, addToFavorites } = props;
   const listItems = items.map((item, index) => (
-    <ul className="item-container">
-      <img key={item.imdbID} srcSet={item.Poster} alt={item.Title} />
-      <li key={item.Website}>{item.Title}</li>
-      <li key={item.Website}>{item.Website}</li>
-    </ul>
+    <Movie
+      key={index}
+      id={item.imdbID}
+      title={item.Title}
+      poster={item.Poster}
+      getMovieInfo={getMovieInfo}
+      addToFavorites={addToFavorites}
+      imdbid={item.imdbID}
+    />
   ));
   return <div id="movie-container">{listItems}</div>;
 };
 
 export default Movies;
+
+/*   <ul className="item-container">
+      <img key={item.imdbID} srcSet={item.Poster} alt={item.Title} onClick={getMovieData} />
+      <li key={item.Website}>{item.Title}</li>
+      <li key={item.Website}>{item.Website}</li>
+    </ul>
+  */
