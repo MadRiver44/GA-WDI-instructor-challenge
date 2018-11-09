097addd8a7e4fb9040a6211cc94c479c5a366119
Kevin Turney <super996@optonline.net>
2018-11-09 11:17:13 -0500

    pass props to Modal

--- client/src/components/Movie.js
+++ client/src/components/Movie.js
@@ -1,23 +1,30 @@
 import React from 'react';
+import Modal from './Modal';
 
 const Movie = props => {
   console.log(props);
-  const { id, imdbID, title, poster, getMovieInfo, addToFavorites } = props;
+  const { id, imdbID, title, poster, getMovieInfo, addToFavorites, isOpen, toggleModal, closeModal } = props;
+  console.log(isOpen);
   return (
-    <li className="movie" id={id}>
-      <img srcSet={poster} alt={title} imdbid={imdbID} selector="on" />
-      <div className="movie-title">{title}</div>
-      <button
-        type="submit"
-        className="button-small"
-        onClick={getMovieInfo}
-        imdbid={imdbID}
-        title={title}
-        id={id}>
-        {' '}
-        FAV
-      </button>
-    </li>
+    <React.Fragment>
+      <li className="movie" id={id}>
+        <img srcSet={poster} alt={title} imdbid={imdbID} selector="on" />
+        <div className="movie-title">{title}</div>
+        <button
+          type="submit"
+          className="button-small"
+          onClick={(getMovieInfo, toggleModal)}
+          imdbid={imdbID}
+          title={title}
+          id={id}>
+          {' '}
+          FAV
+        </button>
+      </li>
+      <Modal className="modal" selector="#modal-root" isOpen={isOpen} onClick={closeModal}>
+        <div className="modal-1">Modal 1 is open</div>
+      </Modal>
+    </React.Fragment>
   );
 };
 
