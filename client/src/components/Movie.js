1b61c179122fbbbc035b8de77e402e7ece693267
Kevin Turney <super996@optonline.net>
2018-11-09 09:34:48 -0500

    add div to surround title

--- client/src/components/Movie.js
+++ client/src/components/Movie.js
@@ -1,18 +1,16 @@
 import React from 'react';
 
 const Movie = props => {
+  console.log(props);
   const { id, imdbID, title, poster, getMovieInfo, addToFavorites } = props;
   return (
     <li className="movie" id={id}>
       <img srcSet={poster} alt={title} imdbid={imdbID} selector="on" />
-
-      <div className="movie-item-container">
-        <p>{title}</p>
-        <button className="button-small" onClick={getMovieInfo}>
-          {' '}
-          FAV
-        </button>
-      </div>
+      <div className="movie-title">{title}</div>
+      <button type="submit" className="button-small" onClick={getMovieInfo} imdbid={imdbID}>
+        {' '}
+        FAV
+      </button>
     </li>
   );
 };
